910983e1fd2fc94d89a5a8068c09965b6133773b
Sravan921 <sukhavasi92@gmail.com>
2016-11-28 23:04:41 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+// Creates an employee array with different values
+
+var employees = [
+    {
+   name:"Sravan Kumar",
+	age:25,
+	salary:12000,
+   address: {
+    city:"Omaha",
+	state:"Nebraska",
+	zipcode:"68164"
+	}
+    }, 
+    {
+     name:"Tirupati Rao",
+	age:21,
+	salary:100000,
+   address: {
+    city:"Omaha",
+	state:"Nebraska",
+	zipcode:"68106"
+	}
+    },
+    {
+     name:"Maruthi Manoj",
+	age:55,
+	salary:50000,
+   address: {
+    city:"Linclon",
+	state:"Nebraska",
+	zipcode:"68100"
+	}
+    },
+	
+	{
+     name:"Abhimanyu",
+	age:45,
+	salary:2000,
+   address: {
+    city:"Des Moines",
+	state:"Iowa",
+	zipcode:"61804"
+	}
+    },
+	
+	{
+     name:"Vijay Sai Jampani",
+	age:18,
+	salary:10000,
+   address: {
+    city:"Los Angeles",
+	state:"California",
+	zipcode:"10007"
+	}
+    }
+];
